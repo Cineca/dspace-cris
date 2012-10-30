42efc7501aea691e298ccdcba45f8f7f426da6b2
Pascarelli Luigi Andrea <pascarelli@cilea.it>
2012-10-30 18:47:29 +0100

    add cris pointer controller

---
+++ jspui-api/src/main/java/it/cilea/hku/authority/webui/controller/jdyna/CrisSearchPointerController.java
@@ -0,0 +1,16 @@
+package it.cilea.hku.authority.webui.controller.jdyna;
+
+import it.cilea.osd.jdyna.web.controller.SearchPointerController;
+
+public class CrisSearchPointerController extends SearchPointerController
+{
+        
+    @Override
+    protected Object getResult(Class target, String filtro, String parentID,
+            String query)
+    {
+        // TODO Auto-generated method stub
+        return super.getResult(target, filtro, parentID, query);
+    }
+    
+}
