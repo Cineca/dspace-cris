a7f10a05c3b3911f10af7b90b905a043306f19ee
fmignogna <fmignogna@kion.it>
2014-01-23 09:51:42 +0100

    SURES-13 Update Hibernate Session Factory

--- api/src/main/java/org/dspace/app/cris/util/Researcher.java
+++ api/src/main/java/org/dspace/app/cris/util/Researcher.java
@@ -22,6 +22,7 @@ import org.dspace.app.cris.statistics.service.StatSubscribeService;
 import org.dspace.utils.DSpace;
 import org.hibernate.SessionFactory;
 import org.springframework.orm.hibernate3.annotation.AnnotationSessionFactoryBean;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 
 public class Researcher
 {
@@ -88,7 +89,7 @@ public class Researcher
     
     public SessionFactory getSessionFactory()
     {
-        return (SessionFactory) dspace.getServiceManager().getServiceByName("&sessionFactory", AnnotationSessionFactoryBean.class).getObject();
+        return (SessionFactory) dspace.getServiceManager().getServiceByName("&sessionFactory", LocalSessionFactoryBean.class).getObject();
     }
     
     public RelationPreferenceService getRelationPreferenceService() {
